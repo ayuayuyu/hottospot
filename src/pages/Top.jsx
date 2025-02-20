bfe0c82f8cf931f79f0234ae14742e4531af9bfd
tanaka00005 <x23054xx@aitech.ac.jp>
2025-02-20 11:27:23 +0900

    topページの変更

--- src/pages/Top.jsx
+++ src/pages/Top.jsx
@@ -2,16 +2,33 @@ import { Link } from "react-router-dom";
 
 import Title from "../layout/home/Title";
 import Background from "../layout/home/background";
+import { useNavigate } from "react-router-dom";
 
 function Top() {
+  const navigate = useNavigate();
+
+  const handleRanking = () => {
+    navigate("/ranking");
+    console.log("ranking");
+  };
+
+  const handleMap = () => {
+    navigate("/map");
+  };
+
   return (
     <div>
-      <Link to="/login">
-        <Title />
-        <Background />
-      </Link>
-      <Link to="/ranking">Ranking</Link>
-      <Link to="/map">Map</Link>
+      <button onClick={handleRanking} style={{ zIndex: 30, position:"absolute", left: "50px", width:"30%"}}>
+        Ranking
+      </button>
+      <button onClick={handleMap} style={{ zIndex: 30, position:"absolute",right: "50px", width:"30%"}}>Map</button>
+
+      
+        <Link to="/login" style={{ zIndex: 10 }}>
+          <Title />
+          <Background />
+        </Link>
+      
     </div>
   );
 }

4f2eefe0431dbf78f1507272cb685ee10e14ebe8
kkkk0113 <koto1658@gmail.com>
2025-02-21 16:54:49 +0900

    profile画面の作成

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,9 @@ import Map from "./pages/Map";
 import Ranking from "./pages/Ranking";
 import Login from "./pages/Login";
 import FriendsModalSheet from "./components/friendsModalSheet/FriendsModalSheet";
-import './App.css';
-import Auth from './firebase/Auth';
+import "./App.css";
+import Auth from "./firebase/Auth";
+import Profile from "./pages/Profile";
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
           <Route path="/map" element={<Map />} />
           <Route path="/ranking" element={<Ranking />} />
           <Route path="/friendsmodal" element={<FriendsModalSheet />} />
+          <Route path="/profile" element={<Profile />} />
           <Route path="*" element={<h1>Not Found Page</h1>} />
         </Routes>
       </BrowserRouter>

da34574a9250cbbe15521b881627b292564a8071
tanaka00005 <x23054xx@aitech.ac.jp>
2025-02-28 18:41:54 +0900

    ページの追加

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
-import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Top from './pages/Top';
-import Map from './pages/Map';
-import Ranking from './pages/Ranking';
-import Login from './pages/Login';
-import FriendsModalSheet from './components/friendsModalSheet/FriendsModalSheet';
-import './App.css';
-import Auth from './firebase/Auth';
-import Profile from './pages/Profile';
-import Element from './pages/Element';
-import AddFriends from './pages/AddFriends';
-import DisplayQrCode from './pages/DisplayQrCode';
-import QrCodeScan from './pages/QrCodeScan';
-import NotFound from './pages/NotFound';
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Top from "./pages/Top";
+import Map from "./pages/Map";
+import Ranking from "./pages/Ranking";
+import Login from "./pages/Login";
+import FriendsModalSheet from "./components/friendsModalSheet/FriendsModalSheet";
+import "./App.css";
+import Auth from "./firebase/Auth";
+import Profile from "./pages/Profile";
+import Element from "./pages/Element";
+import AddFriends from "./pages/AddFriends";
+import DisplayQrCode from "./pages/DisplayQrCode";
+import QrCodeScan from "./pages/QrCodeScan";
+import NotFound from "./pages/NotFound";
+import AlbumMap from "./pages/AlbumMap";
+
 // import AlbumMap from "./pages/AlbumMap";
 
 function App() {
@@ -33,7 +35,7 @@ function App() {
           <Route path="/profile" element={<Profile />} />
           <Route path="/addfriends" element={<AddFriends />} />
           <Route path="/scanqr" element={<QrCodeScan />} />
-          {/* <Route path="/albummap" element={<AlbumMap />} /> */}
+          <Route path="/albummap" element={<AlbumMap />} />
         </Routes>
       </BrowserRouter>
     </>
